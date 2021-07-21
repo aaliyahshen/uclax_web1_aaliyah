9929bf61bb95e101a5af543b6875c8eaa53fb899
Aaliyah Shen <shenfaith0622@gmail.com>
2021-07-21 16:41:27 -0700

    Building Layout Components

--- src/React/Homework/Homework.jsx
+++ src/React/Homework/Homework.jsx
@@ -27,7 +27,23 @@ const Homework = () => {
             number={ 2 }
             question='Explain the difference between HTML, CSS and JS'
         >
-            My answer is as follows 
+            <p>
+            HTML (structure) stands for HyperText Markup Langauge. It was developed in 1990. It provides the structural information of websites, creates hierarchy and delivers content. Using HTML, web developers can add headings, format paragraphs, control line breaks, make lists, emphasize text, create special characters, insert images, create links, build tables, control some styling, and much more.
+            Developers do all these things through markup language, which includes tags or element that are used to identify different types of content. For example, there are opening paragraph tags and closing paragraph tags for  paragraph content.
+            </p>
+            <p>
+            CSS (styling) stands for Cascading Style Sheets. While HTML gives the basic and raw structure of the website, CSS is the one that gives the entire website its style (brand presence, visual hierarchy, defines values). It does so by assigning different properties to HTML tags, either specific to single tags, multiple tags, an entire document, or multiple documents. It was developed in 1996 with the purpose to allow the web designers to adopt design elements such as fonts and colors. The name "cascading style sheets" means that the style sheet developed by web designers can override the default style sheet that comes with the browser.
+            </p>
+            <p>Java Script (behavior) was released in 1995. It is more complicated than HTML and CSS. It is a programming langauge that lets web developers create interactive websites. It modifies website content and makes the website behave in different ways based on users' actions. Some very basic examples of JavaScript in action include confirmation box or slide-in call-to-action (CTA). Others include security password creation, check forms, interactive games, animations and special effects.  
+            </p>
+        </Essay>
+        <Essay
+            number={ 3 }
+            question='What are Single Page Apps (SPAs)? How do they differ from traditional multi-page Web Sites?'
+        >
+            <p>
+            my answers are as follows
+            </p>
         </Essay>
     </div>
     ); 
