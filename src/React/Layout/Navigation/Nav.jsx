bfe2f48158f2b756d7334f792ccb67bbd4d6841f
Aaliyah Shen <shenfaith0622@gmail.com>
2021-08-13 16:29:24 -0700

    adding hamburger icon

--- src/React/Layout/Navigation/Nav.jsx
+++ src/React/Layout/Navigation/Nav.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 import styled from 'styled-components';
 import { NavLink} from 'react-router-dom';
 
+/* Scripts ---------------------------*/
+import { mediaQueries } from 'common/mediaQueries/mediaQueries.js';
+
 const Nav = () => {
     return (
-         <NavStyled>
+        <NavStyled>
             <NavLink to="/" exact>Welcome</NavLink>
             <NavLink to="/services">Services</NavLink>
             <NavLink to="/contact">Contact</NavLink>
@@ -18,17 +21,16 @@ export default Nav;
 
 const NavStyled = styled.nav`
     background-color: #005050;
-    color: white;
     padding: 10px; 
     text-align: center;
 
     a {
-        display: inline-block;
+        display: block;
         background-color: teal;
         color: white;
         padding: 10px; 
         border-radius: 5px; 
-        margin: 0px 10px; 
+        margin: 5px 0px; 
 
         text-decoration: none; 
         text-transform: uppercase;
@@ -41,6 +43,15 @@ const NavStyled = styled.nav`
         &.active {
             color: #003c3c;
             background-color: #00baba;
+        }
+    }
+
+    @media ${mediaQueries.mdUp} {
+        a {
+            display: inline-block;
+            margin: 0px 10px; 
+            font-size: 16px;
+            border-radius: 5px; 
         }
     }
 `;
